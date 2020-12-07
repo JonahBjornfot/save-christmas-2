6d705e59d0afeb19767a23adb14ffd9bc0e247ae
Jonah Björnfot <65647056+JonahBjornfot@users.noreply.github.com>
2020-12-07 14:58:35 +0100

    Fått wsl och fungera

---
+++ playground.js
@@ -0,0 +1,23 @@
+import { randomArray } from './helpers.js'
+
+const a = randomArray(100, 10);
+
+const b = [10, 40, 20, 54, 93, 72, 54, 34, 76]
+b.sort();
+console.log(b);
+
+const storage = [
+    'Bicycle',
+    'Lego',
+    'Doll',
+    'Teddybear',
+    'Crayons',
+    'Fishing rod'
+]
+
+storage.sort();
+console.log(storage);
+
+const month = ['Feb', 'March', 'Dec', 'Jan'];
+month.sort();
+console.log(month);
